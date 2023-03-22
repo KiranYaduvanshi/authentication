5a3f2dcb268de80a0b0066d04d717d08d71145b7
kiranyaduvanshi <kirandeveloper786@gmail.com>
2023-03-22 23:37:23 +0530

    Initial commit

---
+++ lib/feature/facebook_login.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+class FacebookManager {
+
+  /// Facebook Login
+  Future<User?> signInWithFacebook() async {
+    try {
+      // Trigger the sign-in flow
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+
+      print("login result ----- ${loginResult.message}");
+
+      // Create a credential from the access token
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithCredential(facebookAuthCredential);
+      User? user = userCredential.user;
+      print("User Email --- ${user!.email}");
+      // Once signed in, return the UserCredential
+      return user;
+    } catch (e) {}
+  }
+}
